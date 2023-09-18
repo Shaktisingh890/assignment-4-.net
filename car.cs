c1acf527b7ec4b96c9b853d9a2a5cb24e9971ce0
Shakti <shaktipp890@gmail.com>
2023-09-18 20:49:41 +0530

    first commit

---
+++ car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    internal class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+
+        public Car(String make,string model,int year)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+
+        public string Data
+        {
+            get {
+                return $"Car Details is {make}, {model}, {year}";
+            
+            }
+            
+        
+        
+        }
+       public static void Main(String[] args)
+        {
+            Car c1 = new Car("Tata", "Nexa", 2005);
+            Console.WriteLine(c1.Data);
+        }
+
+    }
+
+   
+}
---
+++ enum_permission.cs
@@ -0,0 +1,50 @@
+﻿using assignment4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    namespace assignment4
+    {
+        [Flags]
+        enum Permissions
+        {
+            None = 0,
+            Read = 1 << 0,  // 1
+            Write = 1 << 1, // 2
+            Execute = 1 << 2 // 4
+        }
+
+        public  class enum_permission
+        {
+           public static void main(string[] args) 
+            {
+                Permissions userPermissions = Permissions.Read | Permissions.Write; // User has both read and write permissions
+
+                // Check if a user has specific permissions
+                if (userPermissions.HasFlag(Permissions.Read))
+                {
+                    Console.WriteLine("User has read permission.");
+                }
+
+                if (userPermissions.HasFlag(Permissions.Write))
+                {
+                    Console.WriteLine("User has write permission.");
+                }
+
+                if (userPermissions.HasFlag(Permissions.Execute))
+                {
+                    Console.WriteLine("User has execute permission.");
+                }
+                else
+                {
+                    Console.WriteLine("User does not have execute permission.");
+                }
+            }
+        }
+    }
+
+
